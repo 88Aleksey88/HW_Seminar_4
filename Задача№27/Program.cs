2538887b561ed733904cb0fad98066af8999281d
Aleksey <Sad_libra1988@mail.ru>
2023-03-19 19:29:13 +0700

    Задача №27

---
+++ Задача№27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int f(int number)
+{
+    int result = 0;
+    while (number > 0)
+        {
+        result = result + (number % 10); 
+        number /=10;
+        }       
+    return result;
+}
+Console.WriteLine(f(number));
