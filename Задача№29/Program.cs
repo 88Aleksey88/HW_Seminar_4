2c45b691a730d5e027abac99734089b388760e57
Aleksey <Sad_libra1988@mail.ru>
2023-03-19 19:31:09 +0700

    Задача №29

---
+++ Задача№29/Program.cs
@@ -0,0 +1,9 @@
+﻿void InputArray (string []array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = Console.ReadLine();
+}
+Console.Clear();
+string []array = new string[8];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
