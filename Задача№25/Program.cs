ea534d7ca1676f4d1b9336b545f0aa64ccd3a593
Aleksey <Sad_libra1988@mail.ru>
2023-03-19 19:27:25 +0700

    Задача №25

---
+++ Задача№25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число:");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число:");
+double number2 = Convert.ToDouble(Console.ReadLine());
+double f(double number1, double number2)
+{
+    double result = Math.Pow(number1, number2);
+    return result;
+}
+Console.WriteLine(f(number1,number2));
